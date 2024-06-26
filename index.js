e060e37b48fd64a78aab12f3b23d3ea941c29680
Muhammadalansari356 <Muhammadalansari356@gmail.com>
2024-06-26 22:58:07 +0300

    seventh

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const path = require('path');
 
 
 //set directory dirname 
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
+// const __dirname = path.dirname(fileURLToPath(import.meta.url))
 dotenv.config({ path: path.join(__dirname, './config/.env') })
 // import express from 'express'
 // import initApp from './src/index.router.js'
@@ -29,4 +29,7 @@ initApp(app ,express)
 console.log(new Date());
 app.listen(port, () => console.log(chalk.blueBright(`Example app listening on port ${port}!`)))
 
+app.use("/" , (req,res)=> {
+return res.send("Hello World")
+})
 module.exports =  app 
