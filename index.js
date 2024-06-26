1d176af63f613d1a3805c256a6cef0d959a29d58
Muhammadalansari356 <Muhammadalansari356@gmail.com>
2024-06-26 22:46:43 +0300

    sixth

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
-import path from 'path'
-import { fileURLToPath } from 'url'
-import dotenv from 'dotenv'
-import chalk from 'chalk' 
+// import path from 'path'
+// import { fileURLToPath } from 'url'
+// import dotenv from 'dotenv'
+// import chalk from 'chalk' 
+
+const path = require('path');
+ const { fileURLToPath }  = require('url');
+ const dotenv = require('dotenv');
+ const chalk = require('chalk');
+ const express = require('express');
+ const initApp = require('./src/index.router.js');
+
+
+
+
+
 //set directory dirname 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 dotenv.config({ path: path.join(__dirname, './config/.env') })
-import express from 'express'
-import initApp from './src/index.router.js'
+// import express from 'express'
+// import initApp from './src/index.router.js'
 // import { date } from 'joi'
 const app = express()
 // setup port and the baseUrl
